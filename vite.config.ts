af2720c51a932dc5186dc8da3e94852fbdaa29c8
Swaroop Ingavale <73553311+swarpya@users.noreply.github.com>
2024-11-03 22:03:07 -0500

    Configured for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-base: '/Gant-Chart-Concept/
+base: '/Gant-Chart-Concept/,
   plugins: [react()],
 });
